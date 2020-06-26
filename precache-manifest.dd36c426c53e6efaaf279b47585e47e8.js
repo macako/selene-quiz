af2f7e4d4caba8b7cd8e7545e646ea5127eb1fd3
macako <imacako@gmail.com>
2020-06-26 12:07:56 -0500

    Updates

---
+++ precache-manifest.dd36c426c53e6efaaf279b47585e47e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0374bf0540cdfc0b93e699caf72f5d5",
+    "url": "/selene-quiz/index.html"
+  },
+  {
+    "revision": "d6a8ccbb6ab71d87c177",
+    "url": "/selene-quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0503a574f5b694074055",
+    "url": "/selene-quiz/static/js/2.40919ce4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/selene-quiz/static/js/2.40919ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a8ccbb6ab71d87c177",
+    "url": "/selene-quiz/static/js/main.67200e61.chunk.js"
+  },
+  {
+    "revision": "1cf238e13d8efa017ba6",
+    "url": "/selene-quiz/static/js/runtime-main.7b3101bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/selene-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
