a873539a560c929405e9772d1992b0baba41581d
macako <imacako@gmail.com>
2020-07-06 23:04:53 -0500

    Renaming app properly from web browser

--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,54 +1,57 @@
-import {
-    ActionTypes
-} from '../constants/actionTypes';
+import { ActionTypes } from "../constants/actionTypes";
 
 let initialQuiz = {
-    quiz: {
-        config: {
-            'allowBack': true,
-            'allowReview': true,
-            'autoMove': false,  // if true, it will move to next question automatically when answered.
-            'duration': 0,  // indicates the time in which quiz needs to be completed. 0 means unlimited.
-            'pageSize': 1,
-            'requiredAll': false,  // indicates if you must answer all the questions before submitting.
-            'richText': false,
-            'shuffleQuestions': false,
-            'shuffleOptions': false,
-            'showClock': false,
-            'showPager': true,
-            'theme': 'none'
-        },
-        questions: []
+  quiz: {
+    config: {
+      allowBack: true,
+      allowReview: true,
+      autoMove: false, // if true, it will move to next question automatically when answered.
+      duration: 0, // indicates the time in which quiz needs to be completed. 0 means unlimited.
+      pageSize: 1,
+      requiredAll: false, // indicates if you must answer all the questions before submitting.
+      richText: false,
+      shuffleQuestions: false,
+      shuffleOptions: false,
+      showClock: false,
+      showPager: true,
+      theme: "none",
     },
-    mode: 'quiz',
-    pager: {
-        index: 0,
-        size: 1,
-        count: 1
-    }
-}
+    questions: [],
+  },
+  mode: "quiz",
+  pager: {
+    index: 0,
+    size: 1,
+    count: 1,
+  },
+};
 
 export default (state = { ...initialQuiz }, action) => {
-    switch (action.type) {
-        case ActionTypes.PagerUpdate:
-            return {
-                ...state, pager: action.payload, mode: 'quiz'
-            }
-        case ActionTypes.QuizLoad:
-            return {
-                ...state, quiz: action.payload
-            }
-        case ActionTypes.QuizSubmit:
-            console.log('quiz.js Submitted.');
-            return {
-                ...state, mode: action.payload
-            }
-        case ActionTypes.QuizAnswer:
-            console.log('quiz.js reducer Quiz Answer called.');
-            return {
-                ...state, quiz: action.payload
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ActionTypes.PagerUpdate:
+      return {
+        ...state,
+        pager: action.payload,
+        mode: "quiz",
+      };
+    case ActionTypes.QuizLoad:
+      return {
+        ...state,
+        quiz: action.payload,
+      };
+    case ActionTypes.QuizSubmit:
+      console.log("quiz.js Submitted.");
+      return {
+        ...state,
+        mode: action.payload,
+      };
+    case ActionTypes.QuizAnswer:
+      console.log("quiz.js reducer Quiz Answer called.");
+      return {
+        ...state,
+        quiz: action.payload,
+      };
+    default:
+      return state;
+  }
 };
